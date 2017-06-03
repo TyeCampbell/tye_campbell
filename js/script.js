93f141b4fcb582225ee3480bf540a5a44b988e2e
Tye Campbell <tylerjoelcampbell@gmail.com>
2017-06-02 21:14:47 -0500

    Project Refresh overall look and feel

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#portfoliolink").mouseover(function(){
+		$("body").css("background-image", "url('images/tye-portfolio-bw.png')");
+	});
+	$("#aboutlink").mouseover(function(){
+		$("body").css("background-image", "url('images/tye-about-bw.png')");
+	});
+	$("#contactlink").mouseover(function(){
+		$("body").css("background-image", "url('images/tye-contact-bw.png')");
+	});
+});
