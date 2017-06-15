1078134cb7854cf62e0af7e0dfde3a36072e3f30
Tye Campbell <tylerjoelcampbell@gmail.com>
2017-06-14 19:41:35 -0500

    Building working links, portfolio, about, and contact to open and close

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$("#portfoliolink").mouseover(function(){
 		$("body").css("background-image", "url('images/tye-portfolio-bw.png')");
 	});
+
 	$("#aboutlink").mouseover(function(){
 		$("body").css("background-image", "url('images/tye-about-bw.png')");
 	});
@@ -9,3 +10,31 @@ $(document).ready(function(){
 		$("body").css("background-image", "url('images/tye-contact-bw.png')");
 	});
 });
+
+$(document).ready(function(){
+	$("#portfoliolink").click(function(){
+		$("#homepage").hide();
+		$("#contactpage").hide();		
+		$("#aboutpage").hide();
+		$("#portfoliopage").show();
+	});
+});
+
+$(document).ready(function(){
+	$("#aboutlink").click(function(){
+		$("#homepage").hide();
+		$("#portfoliopage").hide();
+		$("#contactpage").hide();		
+		$("#aboutpage").show();
+	});
+});
+
+$(document).ready(function(){
+	$("#contactlink").click(function(){
+		$("#homepage").hide();
+		$("#portfoliopage").hide();
+		$("#aboutpage").hide();
+		$("#contactpage").show();		
+	});
+});
+
