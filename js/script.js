9a6f2f4b2c58f8945aab4abebadd26113f44a90a
Tye Campbell <tylerjoelcampbell@gmail.com>
2017-07-15 14:20:31 -0500

    finished porfolio links. added email parser.php and jQuery for contact form. Added css for validation colors.

--- js/script.js
+++ js/script.js
@@ -93,9 +93,191 @@ $(document).ready(function(){
 // Preview - Sandy's Pet Shop
 
 $(document).ready(function(){
-	$("#preview-honeyhill").click(function(){
-		$("#previewTitle").text("Honey Hill Events");
-		$("#previewSite").attr("src","http://www.honeyhillevents.com");
-		$(".previewLink").attr("href","http://www.honeyhillevents.com");
+	$("#preview-sandy").click(function(){
+		$("#previewBody-honeyhill").hide();
+		$("#previewBody-objectivebox").hide();
+		$("#previewBody-sandyspet").show();
+		$("#previewTitle").text("Sandy's Pet Shop");
+		$("#previewSite").attr("src","http://www.tyecampbell.com/sandyspetshop/");
+		$(".previewLink").attr("href","http://www.tyecampbell.com/sandyspetshop/");
 	});
 });
+
+
+// Preview - Sandy's Pet Shop Admin Page
+
+ $(document).ready(function(){
+	$("#preview-sandy-admin").click(function(){
+		$("#previewSite").attr("src","http://www.tyecampbell.com/sandyspetshop/admin.php");
+	});
+});
+
+
+
+
+/* ---------- Regular Expressions For Frorm Validations ---------- */
+
+var reProperName = /^[a-z ,.'-]+$/i;
+var reEmail = /^(\w+[\-\.])*\w+@(\w+\.)+[A-Za-z]+$/;
+
+
+
+/* ---------- Contact Us Form ---------- */
+
+
+
+/* Validate First and Last Names */
+
+$("#fname").blur(function(){
+	if ($(this).val() == "") {
+		$(this).parent().addClass("has-error");
+		$(this).next("span").addClass("glyphicon-remove");
+		$(this).next("span").next().html("Required information.")
+		$("#sendmessage").addClass("disabled");
+
+	} else if (!reProperName.test($(this).val())) {
+		$(this).parent().addClass("has-error");
+		$(this).next("span").addClass("glyphicon-remove");
+		$(this).next("span").next().html("Not a valid name.")
+		$("#sendmessage").addClass("disabled");
+	} else {
+		$(this).parent().removeClass("has-error");
+		$(this).next("span").removeClass("glyphicon-remove");
+		$(this).next("span").next().html("");
+		$("#sendmessage").removeClass("disabled");
+	};
+});
+
+
+/* Validate Email Address */
+
+$("#email").blur(function(){
+if ($(this).val() == "") {
+		$(this).parent().addClass("has-error");
+		$(this).next("span").addClass("glyphicon-remove");
+		$(this).next("span").next().html("Email address required.")
+		$("#sendmessage").addClass("disabled");
+
+	} else if (!reEmail.test($(this).val())) {
+		$(this).parent().addClass("has-error");
+		$(this).next("span").addClass("glyphicon-remove");
+		$(this).next("span").next().html("Not a valid email address")
+		$("#sendmessage").addClass("disabled");
+	} else {
+		$(this).parent().removeClass("has-error");
+		$(this).next("span").removeClass("glyphicon-remove");
+		$(this).next("span").next().html("");
+		$("#sendmessage").removeClass("disabled");
+	};
+});
+
+/* Validate Message Body Address */
+
+
+$("#message").blur(function(){
+		if ($(this).val() == "") {
+			$(this).parent().addClass("has-error");
+			$(this).next("span").addClass("glyphicon-remove");
+			$(this).next("span").next().html("Required information.")
+			$("#sendmessage").addClass("disabled");
+		}  else if ($(this).val().length < 5) {
+			$(this).parent().addClass("has-error");
+			$(this).next("span").addClass("glyphicon-remove");
+			$(this).next("span").next().html("More information is required. Keep typing...")
+			$("#sendmessage").addClass("disabled");
+		} else if ($(this).val().length > 1000) {
+			$(this).parent().addClass("has-error");
+			$(this).next("span").addClass("glyphicon-remove");
+			$(this).next("span").next().html("You typed " + $(this).val().length + " characters. Maximum message limit is 1000 characters.")
+			$("#sendmessage").addClass("disabled");
+		} else {
+			$(this).parent().removeClass("has-error");
+			$(this).next("span").removeClass("glyphicon-remove");
+			$(this).next("span").next().html("");
+			$("#sendmessage").removeClass("disabled");
+		};
+});
+
+
+function submitForm() {
+	
+	var $firstName = $("#fname");
+	var $email = $("#email");
+	var $message = $("#message");
+	
+	function submitValidate(element) {
+		$(element).parent().addClass("has-error");
+		$(element).next("span").addClass("glyphicon-remove");
+		$(element).next("span").next().html("Required information.");
+	};
+
+	if ($firstName.val() == "" || $email.val() == "" || $message.val() == "" || $message.val().length < 5) {
+		
+		$("#sendmessage").addClass("disabled");
+
+		if ($firstName.val() == "") {
+			submitValidate($firstName);
+		};
+
+		if ($email.val() == "") {
+			submitValidate($email);
+		};
+
+		if ($message.val() == "" || $message.val().length < 5){
+			submitValidate($message);
+		};				
+	} else {
+		sendForm();
+	}
+}
+
+/* SendForm via AJAX*/
+
+function sendForm() {
+		
+	$("#sendmessage").addClass("disabled");
+
+	var $firstName = $("#fname");
+	var $email = $("#email");
+	var $message = $("#message");
+
+	var formdata = new FormData();
+	formdata.append("fname", $firstName.val());
+	formdata.append("email", $email.val());
+	formdata.append("message", $message.val());	
+	var ajax = new XMLHttpRequest();
+	ajax.open("POST", "email_parser.php");
+	ajax.onreadystatechange = function() {
+		if (ajax.readyState == 4 && ajax.status == 200) {
+			if (ajax.responseText == "success") {
+				$('#mymodal').modal({show: true});
+				$("#statusheader").html("Thank you!");		
+				$("#status").html("Thanks for reaching out to us, " + $firstName.val() + "! We will respond to your request as soon as possible.");						
+				$firstName.val("");
+				$email.val("");
+				$message.val("");
+				$("#sendmessage").removeClass("disabled");		
+			} else {
+				$('#mymodal').modal({show: true});				
+				$("#statusheader").html("There was an error sending your message. Please try again later.");				
+				$("#status").html(ajax.responseText);
+				$("#sendmessage").removeClass("disabled");
+			}
+		}
+	}
+	ajax.send(formdata);
+}
+
+/* ---------- Google Map API ---------- */
+
+function initMap() {
+        var uluru = {lat: 41.037955, lng: -93.7810656};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
