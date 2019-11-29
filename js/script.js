2bfa39eadad823b75a186b2a5c0a164d276da58e
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-11-29 10:11:52 -0600

    fixed messed up bracket in function formSuccess

--- js/script.js
+++ js/script.js
@@ -221,22 +221,21 @@ $("#message").blur(function(){
 		};
 });
 
-
 function formSuccess() {
-
-  var $firstName = $("#fname");
-  var $email = $("#email");
-  var $message = $("#message");
-
-  $('#contactModal').modal({show: true});
-  $("#statusheader").html("Thank you!");
-  $("#status").html("Thanks for the message, " + $firstName.val() + "! I will respond to your request as soon as possible.");
-  $firstName.val("");
-  $email.val("");
-  $message.val("");
-  $("#sendmessage").removeClass("disabled");
-}, 
-
+	
+	var $firstName = $("#fname");
+	var $email = $("#email");
+	var $message = $("#message");
+	
+	$('#contactModal').modal({show: true});
+	$("#statusheader").html("Thank you!");
+	$("#status").html("Thanks for the message, " + $firstName.val() + "! I will respond to your request as soon as possible.");
+	$firstName.val("");
+	$email.val("");
+	$message.val("");
+	$("#sendmessage").removeClass("disabled");
+	
+};
 
 $("#contactus").submit(function(e) {
 	e.preventDefault();
@@ -246,7 +245,6 @@ $("#contactus").submit(function(e) {
 	  formSuccess();
 	});
   });
-
 
 
 /* ---------- Google Map API ---------- */
