3235e29c3135f9b7b7b169cf2a791fb00795d5a5
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-11-29 09:55:43 -0600

    added netlify js snippet for alert thank you

--- js/script.js
+++ js/script.js
@@ -221,6 +221,16 @@ $("#message").blur(function(){
 		};
 });
 
+$("#contactus").submit(function(e) {
+	e.preventDefault();
+  
+	var $form = $(this);
+	$.post($form.attr("action"), $form.serialize()).then(function() {
+	  alert("Thank you!");
+	});
+  });
+
+
 /* ---------- Google Map API ---------- */
 
 function initMap() {
